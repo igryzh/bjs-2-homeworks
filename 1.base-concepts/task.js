f2600943107a27eccff8ce5fc4658f1d839f47fb
Igor Ryzhkov <g7766650@gmail.com>
2024-01-06 21:06:04 +0300

    base-concepts ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const discriminant = b ** 2 - 4 * a * c;
+
+  // Проверяем значения дискриминанта и возвращаем соответствующий результат
+  if (discriminant < 0) {
+    // Нет корней
+    return (arr = []);
+  } else if (discriminant === 0) {
+    // Один корень
+    const root = -b / (2 * a);
+    return (arr = [root]);
+  } else {
+    // Два корня
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return (arr = [root1, root2]);
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let result = 0;
+  const monthPay =
+    creditBody *
+    (percentMonth + percentMonth / ((1 + percentMonth) ** countMonths - 1));
+  if (monthPay == 0) {
+    return result;
+  } else {
+    result = Number((countMonths * monthPay).toFixed(2));
+    return result;
+  }
 }
