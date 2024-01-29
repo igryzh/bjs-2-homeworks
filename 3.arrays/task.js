eef994be358f8ac12298e68d61f476541b06df44
Igor Ryzhkov <g7766650@gmail.com>
2024-01-29 22:08:37 +0300

    change

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter((item) => item.gender === gender)
+    .map((item) => item.age)
+    .reduce((acc, item, index, arr) => {
+      const sum = acc + item;
+      if (index === arr.length - 1) {
+        return sum / arr.length;
+      }
+      return sum;
+    }, 0);
+  return result;
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks) {
+    this.marks.push(...marksToAdd);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (this.marks && this.marks.length !== 0) {
+    const result = this.marks.reduce(
+      (sum, number) => sum + number / this.marks.length,
+      0
+    );
+    return result;
+  }
+  return 0;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
